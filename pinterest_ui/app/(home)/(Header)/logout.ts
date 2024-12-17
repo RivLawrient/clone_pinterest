a3ac84a4f86d071d78a52cc61607b9c7f8e4274e
lawrient <sandincl.d@gmail.com>
2024-12-18 04:17:39 +0800

    clean code

--- pinterest_ui/app/(home)/(Header)/logout.ts
+++ pinterest_ui/app/(home)/(Header)/logout.ts
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
 
 export default async function Logout() {
-  const response = await fetch(`${process.env.HOST_API_PUBLIC}/bye`, {
-    credentials: "include",
-  });
+  // const response = await fetch(`${process.env.HOST_API_PUBLIC}/bye`, {
+  //   credentials: "include",
+  // });
+  document.cookie = `${"auth-token"}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
   window.location.reload();
 }
