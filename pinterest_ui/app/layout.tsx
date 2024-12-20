f32b43a26b1a615893d780e2888fe960a04f0830
lawrient <sandincl.d@gmail.com>
2024-12-20 23:06:58 +0800

    clean code

--- pinterest_ui/app/layout.tsx
+++ pinterest_ui/app/layout.tsx
@@ -3,6 +3,7 @@ import { NotifProvider } from "./(notifContext)/Notif";
 import { ModalProvider } from "./@landing/(modalContext)/Modal";
 import "./globals.css";
 import { cookies } from "next/headers";
+import { Metadata } from "next";
 
 export default async function RootLayout({
   landing,
@@ -16,6 +17,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <ModalProvider>
         <NotifProvider>
           <body>{token ? children : landing}</body>
