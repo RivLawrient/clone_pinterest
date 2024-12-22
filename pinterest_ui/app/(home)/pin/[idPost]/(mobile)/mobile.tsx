21350b1dc1a798874418691aca981de87b9ef0bb
lawrient <sandincl.d@gmail.com>
2024-12-23 04:48:41 +0800

    clean code

--- pinterest_ui/app/(home)/pin/[idPost]/(mobile)/mobile.tsx
+++ pinterest_ui/app/(home)/pin/[idPost]/(mobile)/mobile.tsx
@@ -73,7 +73,7 @@ export function Mobile({
     return (
       <div
         onClick={() => setShowMsg(false)}
-        className={`absolute inset-0 z-[5] flex h-full w-screen items-end overflow-hidden bg-black/80 md:hidden`}
+        className={`fixed inset-0 z-[5] flex h-full w-screen items-end overflow-hidden bg-black/80 md:hidden`}
       >
         <div
           onClick={(e) => e.stopPropagation()}
