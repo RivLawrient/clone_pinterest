fba443830f7bb0554d69835de93bfda7df59ada1
lawrient <sandincl.d@gmail.com>
2024-12-23 06:45:46 +0800

    clean code

--- pinterest_ui/app/(home)/pin/[idPost]/(mobile)/mobile.tsx
+++ pinterest_ui/app/(home)/pin/[idPost]/(mobile)/mobile.tsx
@@ -124,7 +124,8 @@ export function Mobile({
           <div
             className={`absolute bottom-0 my-2 flex w-full items-center gap-1 bg-white`}
           >
-            <input
+            <input type="text" placeholder=" ini input" className="bg-black" />
+            {/* <input
               value={comment}
               onChange={(e) => {
                 setComment(e.target.value);
@@ -156,7 +157,7 @@ export function Mobile({
               >
                 <path d="m.46 2.43-.03.03c-.4.42-.58 1.06-.28 1.68L3 10.5 16 12 3 13.5.15 19.86c-.3.62-.13 1.26.27 1.67l.05.05c.4.38 1 .56 1.62.3l20.99-8.5q.28-.12.47-.3l.04-.04c.68-.71.51-2-.51-2.42L2.09 2.12Q1.79 2 1.49 2q-.61.01-1.03.43"></path>
               </svg>
-            </div>
+            </div> */}
             {/* <div
               className={`mx-2 mr-2 flex w-full items-center gap-2 rounded-full border px-4 py-2 text-[16px] text-black`}
             >
