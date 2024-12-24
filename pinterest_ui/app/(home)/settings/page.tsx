602cbd641cab79b2d3f5d34296f9a2043978ce31
lawrient <sandincl.d@gmail.com>
2024-12-25 03:43:55 +0800

    clean code

--- pinterest_ui/app/(home)/settings/page.tsx
+++ pinterest_ui/app/(home)/settings/page.tsx
@@ -6,15 +6,12 @@ export default function SettingsPage() {
 
   return (
     <>
-      <div
-        style={{
-          scrollbarWidth: "none",
-        }}
-        className="fixed flex h-screen w-screen overflow-y-scroll md:mt-20 md:flex-row"
-      >
+      <div className="fixed flex h-screen w-screen overflow-y-scroll md:mt-20 md:flex-row">
         <div className="mt-6 flex h-[2000px] w-full justify-center bg-slate-500 md:basis-2/3 md:justify-end lg:basis-1/2">
           <TopNavbar />
-          settings
+          <div>
+            <input type="text" placeholder="input" className="bg-red-500" />
+          </div>
         </div>
       </div>
       <BottomNavbar />
@@ -32,7 +29,7 @@ function TopNavbar() {
 
 function BottomNavbar() {
   return (
-    <div className="absolute bottom-0 hidden w-screen flex-row bg-red-500 md:flex">
+    <div className="fixed bottom-0 hidden w-screen flex-row bg-red-500 md:flex">
       <div className="flex justify-end md:basis-2/3 lg:basis-1/2">nice</div>
     </div>
   );
