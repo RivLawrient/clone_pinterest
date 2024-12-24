d4885109b3ddbbfc7ae7ddc9743cb5bc90b9a276
lawrient <sandincl.d@gmail.com>
2024-12-25 03:30:10 +0800

    clean code

--- pinterest_ui/app/(home)/settings/page.tsx
+++ pinterest_ui/app/(home)/settings/page.tsx
@@ -6,7 +6,12 @@ export default function SettingsPage() {
 
   return (
     <>
-      <div className="fixed flex h-screen w-screen overflow-y-scroll md:mt-20 md:flex-row">
+      <div
+        style={{
+          scrollbarWidth: "none",
+        }}
+        className="fixed flex h-screen w-screen overflow-y-scroll md:mt-20 md:flex-row"
+      >
         <div className="mt-6 flex h-[2000px] w-full justify-center bg-slate-500 md:basis-2/3 md:justify-end lg:basis-1/2">
           <TopNavbar />
           settings
